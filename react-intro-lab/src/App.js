3d39340cb025423206d5c548f4f21daa3dfadf8d
Dbojarrovs <60179609+Dbojarrovs@users.noreply.github.com>
2021-02-19 11:01:28 +0000

    Finished lab03
    Made 3 cards with lecture names

--- react-intro-lab/src/App.js
+++ react-intro-lab/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Subject from './components/subject';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hi, I'm a React App</h1>
+      <Subject title="Web App Development 2" year="Year 3" lecture="Rosanne Birney">
+     
+      
+      Read more about this subject
+      </Subject>
+      <Subject title="NoSQL Databases " year="Year 3" lecture="T.J. McDonald" >
+        
+        Read more about this subject
+      </Subject>
+      <Subject title="Digital Graphic Design" year="Year 3" lecture="Sinead O'Riordan" >
+        
+        Read more about this subject
+      </Subject>
     </div>
   );
 }
---
+++ react-intro-lab/src/components/subject.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const subject = (props) => {
+
+    
+        return (
+            <div className="card">
+                <p>This is a subject! It is called {props.title} 
+                and it is in {props.year} of the course. The lecture
+                is:</p>
+                <p id="lecture">{props.lecture} </p>
+                <p id="props">{props.children}</p>
+                
+            </div>
+    
+        )
+    
+}
+
+export default subject;
