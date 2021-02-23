5142e515ffd8c4f733ef8147e1c76c2729363ef2
Dbojarrovs <60179609+Dbojarrovs@users.noreply.github.com>
2021-02-23 01:07:35 +0000

    Lab4

--- lab4/src/components/subject.js
+++ lab4/src/components/subject.js
@@ -2,19 +2,18 @@ import React from 'react';
 
 const subject = (props) => {
 
-    
-        return (
-            <div className="card">
-                <p>This is a subject! It is called {props.title} 
-                and it is in {props.year} of the course. The lecture
-                is:</p>
-                <p id="lecture">{props.lecture} </p>
-                <p id="props">{props.children}</p>
-                
-            </div>
-    
-        )
-    
+    return (
+        <div className="card">
+            <p>This is a subject! It is called {props.title} and it is in {props.year} of the course. The lecturer is <span className="lecturer">{props.lecturer}</span></p>
+
+            <p className="children" onClick={props.readmore}>{props.children}</p>
+            <p onClick={props.hide} className="hide">Hide this card</p>
+        </div>
+
+    )
 }
+
+    
+        
 
 export default subject;
