ad53395817296f26e3337e4a64307fb164e78cde
Dbojarrovs <60179609+Dbojarrovs@users.noreply.github.com>
2021-04-03 19:32:53 +0100

    Step 5
    Order Now working

--- pizzapal/src/components/Order/Checkout/Checkout.js
+++ pizzapal/src/components/Order/Checkout/Checkout.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { Grid, Header } from 'semantic-ui-react';
 import OrderSummary from './OrderSummary/OrderSummary';
+import OrderModal from './OrderModal/OrderModal';
 
 const Checkout = (props) => {
-  return (
-    <div>
-          <Grid.Column width={6} textAlign='right'>
-
-<Header as='h2' textAlign='center' className='step'>
-    Step 2: Check out 
-</Header>
-
-<OrderSummary />
-
-</Grid.Column>
-    </div>
-  )
+    return (
+        <Grid.Column width={6} textAlign='right'>
+            <Header as='h2' textAlign='center' className='step'>
+                Step 2: Check out 
+            </Header>
+            <OrderSummary />
+            <OrderModal />
+        </Grid.Column>
+      )
 };
 
 export default Checkout;
