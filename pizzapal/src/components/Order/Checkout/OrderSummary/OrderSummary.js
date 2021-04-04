2fdaa293ec88c4a90a03de16f189cd476ad10470
Dbojarrovs <60179609+Dbojarrovs@users.noreply.github.com>
2021-04-04 19:23:21 +0100

    step 8
    Order summary

--- pizzapal/src/components/Order/Checkout/OrderSummary/OrderSummary.js
+++ pizzapal/src/components/Order/Checkout/OrderSummary/OrderSummary.js
@@ -2,56 +2,49 @@ import React from "react";
 import { Header, List } from 'semantic-ui-react';
 
 const OrderSummary = (props) => {
-// get all the ids of the chosen toppings
-// get all the ids of the chosen toppings
-const toppingIdsArray = [];
-for(let i in props.toppings){
-    toppingIdsArray.push(props.toppings[i].id);
-};
 
-// function to count occurences of each topping
-const countOccurrences = (array, value) => array.reduce((count, num) => (num === value ? count + 1 : count), 0);
+    let summary = null;
 
-// create an empty array for storing the toppings with their counts
-const toppingsSummary = [];
+    if(props.toppings.length > 0){
 
-// loop through and check for all 16 ids
-for(let id=0; id<16; id++){
+        summary = (
+            <div>
+                <Header as='h3'>
+                    Your Pizza: 
+                </Header>
 
-    // use countOccurences to count occurences of each id
-    let toppingCount = countOccurrences(toppingIdsArray, id);
+                <List divided verticalAlign='middle'>
+                    {props.toppings.map((topping) => {
+                        return( 
+                            <List.Item key={topping.id}>
+                                {topping.name}: {topping.count}
+                            </List.Item>
+                        )
+                    })}
+                </List>
 
-     // if a topping has a count more than 0
-     if (toppingCount > 0) {
-  // create a new object for that topping that includes the count
-  const toppingWithCount = {
-    id: id,
-    name: props.menu[id].alt,
-    count: toppingCount
-};
-
-// add the toppingWithCount to the toppingsSummary array
-toppingsSummary.push(toppingWithCount);
+                <Header as='h4' className='h4margin'>
+                    Total Price: &euro; {props.price.toFixed(2)}
+                </Header>
+            </div>
+        );
     }
-}
+    else{
+        summary = (
+            <div>
+                <Header as='h4' className="h4margin">
+                    Start adding some toppings! 
+                </Header>
+            </div>
+        );
+    }
 
 
-  return (
-    <div>
-     <Header as='h4' className='h4margin'>
-      Total Price: &euro; {props.price.toFixed(2)}
-  </Header>
-    <List divided verticalAlign='middle'>
-    {toppingsSummary.map((topping) => {
-          return( 
-              <List.Item key={topping.id}>
-                  {topping.name}
-              </List.Item>
-          )
-      })}
-  </List>
-    </div>
-  )
+    return (
+        <div>
+            {summary}
+        </div>
+    );
 };
 
 export default OrderSummary;
--- pizzapal/src/containers/PizzaPal/PizzaPal.js
+++ pizzapal/src/containers/PizzaPal/PizzaPal.js
@@ -32,50 +32,63 @@ const PizzaPal = (props) => {
       });
      
       const addToppingHandler = (id) => {
-        const index = menuState.toppings.findIndex(topping => topping.id === id);
-      
-      
-        // Save the name and price of the chosen topping
-    const chosenTopping = {
-        id: menuState.toppings[index].id,
-        name: menuState.toppings[index].alt,
-        price: menuState.toppings[index].price
-      };
-
-      console.log(orderState);
-
-       // Add chosen topping object to updatedToppings array
-    orderToppings.push(chosenTopping);
-
-     // Calculate the new price
-     const newPrice = orderState.totalPrice + menuState.toppings[index].price;
-    
-    // Update the order state with the new price and updated toppings array
-    setOrderState({
+        // find the chosen topping in the menu
+        const menuIndex = menuState.toppings.findIndex(topping => topping.id === id);
+  
+      // check if the topping has already been added to the orderToppings array
+      const orderIndex = orderToppings.findIndex(topping => topping.id === id);
+  
+      // if so, increase its count by 1
+      if (orderIndex > -1){
+        orderToppings[orderIndex].count++;
+      }
+      // otherwise (i.e. this topping is being added for the first time)
+      // create this topping and add it to the order toppings array
+      else{
+        // Save the id, name and price of the chosen topping; set its count to 1
+        const chosenTopping = {
+          id: menuState.toppings[menuIndex].id,
+          name: menuState.toppings[menuIndex].alt,
+          price: menuState.toppings[menuIndex].price,
+          count: 1
+        };
+        orderToppings.push(chosenTopping);
+      }
+  
+      // Calculate the new price
+      const newPrice = orderState.totalPrice + menuState.toppings[menuIndex].price;
+  
+      // Update the order state with the new price and updated toppings array
+      setOrderState({
         totalPrice: newPrice,
         chosenToppings: orderToppings
       });
     }
 
     const removeToppingHandler = (id) => {
-        // Find topping with matching id from the orderState
-        const index = orderState.chosenToppings.findIndex(topping => topping.id === id);
-    
-        // Get the current price
-        let price = orderState.totalPrice; 
-    
-        // If topping was found, update the price and then remove it
-        if(index >= 0){
-          price = price - orderState.chosenToppings[index].price;
+      // Find topping with matching id from the orderToppings
+      const index = orderToppings.findIndex(topping => topping.id === id);
+  
+      // Get the current price
+      let price = orderState.totalPrice; 
+  
+      // If topping was found, update the price and decrease the count
+      if(index >= 0){
+        price = price - orderToppings[index].price;
+        orderToppings[index].count--;
+  
+        // If the count is now 0, remove the topping completely
+        if(orderToppings[index].count < 1){
           orderToppings.splice(index, 1);
         }
-    
-        // Update order state with updated price and updated toppings array
-        setOrderState({
-          totalPrice: price,
-          chosenToppings: orderToppings
-        });
       }
+  
+      // Update order state with updated price and updated toppings array
+      setOrderState({
+        totalPrice: price,
+        chosenToppings: orderToppings
+      });
+    }     
 
       console.log(orderState);
 
