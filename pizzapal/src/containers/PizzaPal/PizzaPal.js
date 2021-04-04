04a59fd77263321ca4cf81077caa970ab178b60e
Dbojarrovs <60179609+Dbojarrovs@users.noreply.github.com>
2021-04-04 19:39:18 +0100

    Step 8
    Add table row , table order folders

--- pizzapal/src/containers/PizzaPal/PizzaPal.js
+++ pizzapal/src/containers/PizzaPal/PizzaPal.js
@@ -1,4 +1,5 @@
 
+import { v4 as uuidv4 } from 'uuid';
 import React, { useState, useEffect } from "react";
 import axios from '../../axios-orders';
 import Menu from '../../components/Menu/Menu';
@@ -93,16 +94,49 @@ const PizzaPal = (props) => {
       console.log(orderState);
 
       const checkoutHandler = () => {
-        axios.post('/orders.json', orderState)
+
+        // get order from orderState
+        let order = orderState;
+  
+        // add unique id
+        order.id = uuidv4();
+  
+        // create formatted date
+        let orderDate = new Date();
+  
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  
+        let dayNum = orderDate.getDay();
+        let day = days[dayNum];
+  
+        let monthNum = orderDate.getMonth();
+        let month = months[monthNum];
+  
+        let date = orderDate.getDate();
+        let year = orderDate.getFullYear();
+  
+        // saves date in the format "Fri 19 Mar 2021"
+        let formattedDate = day + " " + date + " " + month + " " + year;
+  
+        // add formattedDate to order
+        order.date = formattedDate;
+  
+        axios.post('/orders.json', order)
         .then(response => {
             alert('Order saved!');
-            console.log(response);
+            // set order state and orderToppings back to starting values
+            setOrderState({
+              totalPrice: 5,
+              chosenToppings: []
+            });
+            orderToppings=[];
         })
         .catch(error => {
-        setMenuState({toppings: menuState.toppings, error: true});
-        alert('Something went wrong :(');
-        console.log(error);
-        });
+          setMenuState({toppings: menuState.toppings, error: true});
+          alert('Something went wrong :(');
+          console.log(error);
+          });
     }
 
    
---
+++ pizzapal/src/containers/YourOrders/YourOrders.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const YourOrders = (props) => {
+  return (
+    <div>
+      <h1>
+Your Orders
+      </h1>
+    </div>
+  )
+};
+
+export default YourOrders;
